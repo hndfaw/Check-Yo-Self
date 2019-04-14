923c422dc520b3a3f4b134e2889a209e866c5477
“Hindreen” <hndfaw@gmail.com>
2019-04-14 07:15:45 -0600

    Correct space of css and js

--- todo-list.js
+++ todo-list.js
@@ -1,49 +1,49 @@
 class Task {
-    constructor(id, title, tasks, urgent) {
-        this.id = id;
-        this.title = title;
-        this.tasks = tasks || [];
-        this.urgent = false;
-    }
+  constructor(id, title, tasks, urgent) {
+    this.id = id;
+    this.title = title;
+    this.tasks = tasks || [];
+    this.urgent = false;
+  }
 
-    saveToStorage(tasksArray) {
-        var strTasksArray = JSON.stringify(tasksArray);
-        localStorage.setItem('tasks', strTasksArray);
-    }
+  saveToStorage(tasksArray) {
+    var strTasksArray = JSON.stringify(tasksArray);
+    localStorage.setItem('tasks', strTasksArray);
+  }
 
-    deleteFromStorage() {
-        var localStorageItems = JSON.parse(localStorage.getItem('tasks'))
-        localStorageItems.splice(tasksArray.indexOf(this),1);
-        localStorage.removeItem('tasks');
-        localStorage.setItem('tasks', JSON.stringify(localStorageItems))
-    }
+  deleteFromStorage() {
+    var localStorageItems = JSON.parse(localStorage.getItem('tasks'))
+    localStorageItems.splice(tasksArray.indexOf(this),1);
+    localStorage.removeItem('tasks');
+    localStorage.setItem('tasks', JSON.stringify(localStorageItems))
+  }
 
 
-    // updateTask() {
+  // updateTask() {
 
-    // }
+  // }
 
-    updateItem(parentId, childElementId) {
-        var localStorageItems = JSON.parse(localStorage.getItem('tasks'))
-        this.tasks.map(item => {
-            (item.id == parseInt(childElementId.dataset.id)) ? item.completed = !item.completed : null;
-          })
-       localStorageItems.splice(tasksArray.indexOf(this),1, this);
-       localStorage.removeItem('tasks');
-       localStorage.setItem('tasks', JSON.stringify(localStorageItems))
-       console.log(localStorage)
+  updateItem(parentId, childElementId) {
+    var localStorageItems = JSON.parse(localStorage.getItem('tasks'))
+    this.tasks.map(item => {
+        (item.id == parseInt(childElementId.dataset.id)) ? item.completed = !item.completed : null;
+      })
+    localStorageItems.splice(tasksArray.indexOf(this),1, this);
+    localStorage.removeItem('tasks');
+    localStorage.setItem('tasks', JSON.stringify(localStorageItems))
+    console.log(localStorage)
 
-    }
+  }
 
-    // updateUrgency() {
-        //  this.urgent = !this.urgent;
-    // }
+  // updateUrgency() {
+  //  this.urgent = !this.urgent;
+  // }
 }
 
 class Item {
-    constructor(id, item) {
-        this.id = id;
-        this.item = item;
-        this.completed = false;
-    }
+  constructor(id, item) {
+    this.id = id;
+    this.item = item;
+    this.completed = false;
+  }
 }
